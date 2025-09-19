c1591623eb5ba4468a5cae8e328227684d39c87b
sungyupjoo <chickenjuicyham@gmail.com>
2025-09-19 18:14:21 +0900

    Finished Gatsby Tutorial Part 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import type { HeadFC, PageProps } from 'gatsby';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout pageTitle="Gatsby practice">
       <p>Gatsby Tutorial</p>
+      <StaticImage alt="Clifford Brown" src="../images/clifford.webp" />
     </Layout>
   );
 };
--- src/types/css-modules.d.ts
+++ src/types/css-modules.d.ts
@@ -1,4 +1,1 @@
-declare module '*.module.css' {
-  const classes: { [key: string]: string };
-  export = classes;
-}
+declare module '*.module.css';
